abbf6baba558dc0210bd05ea9b8752367df67760
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-09 15:56:03 -0700

    Remove console logs from the .checkPrice prototype. This logic passes the test for pizzas with no toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,16 @@ Pizza.prototype.addTopping = function(newTopping) {
 
 Pizza.prototype.checkPrice = function() {
   let numberOfToppings = this.toppings.length;
-  console.log(numberOfToppings);
   this.currentPrice += (numberOfToppings*1.25);
-  console.log(this.currentPrice);
   if (this.size === "small")  {
     this.currentPrice += 5.00;
     return this.currentPrice;
   } else if (this.size === "medium")  {
     this.currentPrice += 9.00;
     return this.currentPrice;
-  } else if (this.size === "large") {
+  } else  {
     this.currentPrice += 13.00;
     return this.currentPrice;
-  } else  {
-    console.log("no pizza size has been selected");
   }
 }
 
@@ -62,7 +58,7 @@ function handleFormSubmission(event) {
     selectedToppingsDiv.append(listElement);
   });
   console.log(newToppingsArray);
-  let newPizza = new Pizza(newToppingsArray, inputtedPizzaSize);
+  const newPizza = new Pizza(newToppingsArray, inputtedPizzaSize);
   console.log(newPizza);
   price = newPizza.checkPrice();
   displayPizzaDetails(newPizza);
